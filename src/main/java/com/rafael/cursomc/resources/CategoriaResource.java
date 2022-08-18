f0080c865a8f6314f6fafb51559a9bb91a3b2316
rafael-henrique-martins <rafael_henrique_martins@outlook.com>
2022-08-17 23:36:39 -0300

    Controller - categoria

---
+++ src/main/java/com/rafael/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,9 @@
+package com.rafael.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "categorias")
+public class CategoriaResource {
+}
