2fdc8af3245010121fb404fe85ac92b2c30bba17
rafael-henrique-martins <rafael_henrique_martins@outlook.com>
2022-08-17 23:36:21 -0300

    Entity - categoria

---
+++ src/main/java/com/rafael/cursomc/domain/Categoria.java
@@ -0,0 +1,25 @@
+package com.rafael.cursomc.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+
+
+}
