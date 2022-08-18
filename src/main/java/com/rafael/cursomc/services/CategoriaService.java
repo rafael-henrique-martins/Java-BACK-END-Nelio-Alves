ebb294f99fb06f2fa90866ebd96ee4772f87512a
rafael-henrique-martins <rafael_henrique_martins@outlook.com>
2022-08-18 00:00:19 -0300

    feat: findById para Service

---
+++ src/main/java/com/rafael/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.rafael.cursomc.services;
+
+import com.rafael.cursomc.domain.Categoria;
+import com.rafael.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria find(Integer id) {
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+
+}
