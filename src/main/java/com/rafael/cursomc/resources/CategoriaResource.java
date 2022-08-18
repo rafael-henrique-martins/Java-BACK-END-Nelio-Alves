cd8c16077f9ca78f40d43dc37b9934cc0212ab4c
rafael-henrique-martins <rafael_henrique_martins@outlook.com>
2022-08-18 00:00:06 -0300

    feat: findById para Controller

--- src/main/java/com/rafael/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rafael/cursomc/resources/CategoriaResource.java
@@ -1,9 +1,23 @@
 package com.rafael.cursomc.resources;
 
+import com.rafael.cursomc.domain.Categoria;
+import com.rafael.cursomc.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "categorias")
 public class CategoriaResource {
+
+    @Autowired
+    private CategoriaService service;
+
+    @RequestMapping(value = "/{id}")
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Categoria obj = service.find(id);
+        return ResponseEntity.ok().body(obj);
+    }
 }
