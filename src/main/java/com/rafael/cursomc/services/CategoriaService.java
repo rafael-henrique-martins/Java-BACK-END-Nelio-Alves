70135cc9dba273acb56d030c4aff745aab8a82b9
rafael-henrique-martins <rafael_henrique_martins@outlook.com>
2022-08-19 00:18:51 -0300

    feat: criando excecao personalizada

--- src/main/java/com/rafael/cursomc/services/CategoriaService.java
+++ src/main/java/com/rafael/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.rafael.cursomc.services;
 
 import com.rafael.cursomc.domain.Categoria;
 import com.rafael.cursomc.repositories.CategoriaRepository;
+import com.rafael.cursomc.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ public class CategoriaService {
 
     public Categoria find(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado id: " + id +
+                ", Tipo: " + Categoria.class.getName()));
     }
-
 }
