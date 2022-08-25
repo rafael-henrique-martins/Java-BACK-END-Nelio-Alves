f60780be58cdaaee9b99c0a31c07f6bc4f230807
rafael-henrique-martins <rafael_henrique_martins@outlook.com>
2022-08-25 00:31:04 -0300

    feat: metodo get para Produto

--- src/main/java/com/rafael/cursomc/resources/ProdutoResource.java
+++ src/main/java/com/rafael/cursomc/resources/ProdutoResource.java
@@ -1,4 +1,23 @@
 package com.rafael.cursomc.resources;
 
+import com.rafael.cursomc.domain.Produto;
+import com.rafael.cursomc.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "produtos")
 public class ProdutoResource {
+
+    @Autowired
+    private ProdutoService service;
+
+    @RequestMapping(value = "/{id}")
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Produto obj = service.find(id);
+        return ResponseEntity.ok().body(obj);
+    }
 }
--- src/main/java/com/rafael/cursomc/services/ProdutoService.java
+++ src/main/java/com/rafael/cursomc/services/ProdutoService.java
@@ -1,4 +1,29 @@
 package com.rafael.cursomc.services;
 
+import com.rafael.cursomc.domain.Cliente;
+import com.rafael.cursomc.domain.Produto;
+import com.rafael.cursomc.repositories.ProdutoRepository;
+import com.rafael.cursomc.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository repo;
+
+
+    public Produto find(Integer id) {
+        Optional<Produto> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado id: " + id +
+                ", Tipo: " + Cliente.class.getName()));
+    }
+
+    public Produto insert(Produto obj){
+        obj.setId(null);
+        return repo.save(obj);
+    }
 }
